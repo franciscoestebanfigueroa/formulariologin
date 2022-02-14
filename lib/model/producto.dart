3e654f75e96cfd9b68cc0b8e4a9d4dac1c6a55cb
Francisco <panchoesteban@gmail.com>
2022-02-13 23:24:41 -0300

    json de respuesta de cloudinary

--- lib/model/producto.dart
+++ lib/model/producto.dart
@@ -45,3 +45,27 @@ class Producto {
     );
   }
 }
+
+
+
+//{
+//    "asset_id": "6e529af803e39621b3cdb5deb5bbc97c",
+//    "public_id": "dzysksqbj5wt3n7w0em7",
+//    "version": 1644805390,
+//    "version_id": "e4afd662706662a043f32adbaf856f36",
+//    "signature": "f12845c69e585ef767891c1e7b9866eec56b4a22",
+//    "width": 1920,
+//    "height": 1080,
+//    "format": "png",
+//    "resource_type": "image",
+//    "created_at": "2022-02-14T02:23:10Z",
+//    "tags": [],
+//    "bytes": 449530,
+//    "type": "upload",
+//    "etag": "8019a5f5ce9139e19589cbf3c0dee9aa",
+//    "placeholder": false,
+//    "url": "http://res.cloudinary.com/dhmgzz9eq/image/upload/v1644805390/dzysksqbj5wt3n7w0em7.png",
+//    "secure_url": "https://res.cloudinary.com/dhmgzz9eq/image/upload/v1644805390/dzysksqbj5wt3n7w0em7.png",
+//    "access_mode": "public",
+//    "original_filename": "relieve"
+//}
